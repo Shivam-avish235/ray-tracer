1a16f8af292298934ef9fb76f8d83a2b301cdf1c
Shivam Verma <vermamavish12035@gmail.com>
2025-11-06 01:16:36 +0530

    camera add

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -4,6 +4,13 @@ in vec2 fPos;
 uniform vec2 WINDOW;
 uniform vec2 center;
 uniform float radius;
+uniform vec2 uResolution;
+uniform vec3 uCameraOrigin;
+uniform float uViewportHeight;
+uniform float uFocalLength;
+
+
+
 
 
 vec2 corrditnateTOndc(vec2 cor){
@@ -15,23 +22,49 @@ vec2 corrditnateTOndc(vec2 cor){
 float normaliseOne(float r){
     return (r / WINDOW.y) * 2.0;
 }
+
+
 void main() {
-     float r2 = normaliseOne(radius);
-     vec2 c2 = corrditnateTOndc(center);
+
+    float aspect_ratio = WINDOW.x / WINDOW.y;
+    float viewport_width = uViewportHeight * aspect_ratio;
+
+    vec3 horizontal = vec3(viewport_width, 0.0, 0.0);
+    vec3 vertical   = vec3(0.0, -uViewportHeight, 0.0);
+    vec3 lower_left = uCameraOrigin
+                    - horizontal / 2.0
+                    - vertical / 2.0
+                    - vec3(0.0, 0.0, uFocalLength);
 
 
-    float dist = distance(fPos,c2);
-    if (dist <= r2)
-        FragColor = vec4(1.0, 0.0, 0.0, 1 -dist / r2); 
-    else
-        FragColor = vec4(0.0, 0.0, 0.0, 1.0); 
-    
+    vec2 ndc = corrditnateTOndc(gl_FragCoord.xy);
+    vec2 uv = (ndc + 1.0) * 0.5;
 
-    // vec2 color = fPos;
-    // float r = (color.x + 1.0) / 2.0;
-    // float g = (  1.0-color.y) / 2.0;+
-    // float b=    0.0;
-    // FragColor = vec4(1.0-fPos.x,0,0, 1.0);
+
+    vec3 ray_dir = lower_left + uv.x * horizontal + uv.y * vertical - uCameraOrigin;
+    ray_dir = normalize(ray_dir);
+
+
+    float t = 0.5 * (ray_dir.y + 1.0);
+    vec3 color = mix(vec3(1.0, 1.0, 1.0), vec3(0.5, 0.7, 1.0), t);
+    FragColor = vec4(color, 1.0);
+
+
+    // float r2 = normaliseOne(radius);
+    // vec2 c2 = corrditnateTOndc(center);
+
+
+    // float dist = distance(fPos,c2);
+    // if (dist <= r2)
+    //     FragColor = vec4(1.0, 0.0, 0.0, 1.0); 
+    // else
+    //     FragColor = vec4(0.0, 0.0, 0.0, 1.0); 
+
+    // float t = (fPos.y + 1.0) / 2.0;
+    // vec3 color = mix(vec3(1.0, 1.0, 1.0), vec3(0.5, 0.7, 1.0), t);
+    // FragColor = vec4(color, 2.0);
+
+
 
 
 }
--- src/game.cpp
+++ src/game.cpp
@@ -3,23 +3,26 @@
 #include <iostream>
 
 float vertices[] = {
-    -1.0f,  1.0f, // top-left
-     1.0f,  1.0f, // top-right
-     1.0f, -1.0f, // bottom-right
-    -1.0f, -1.0f  // bottom-left
+    -1.0f, 1.0f, // top-left
+    1.0f, 1.0f,  // top-right
+    1.0f, -1.0f, // bottom-right
+    -1.0f, -1.0f // bottom-left
 };
 
-unsigned int indices[] = { 0, 1, 2, 2, 3, 0 };
+unsigned int indices[] = {0, 1, 2, 2, 3, 0};
 
-Game::Game(int W_W, int W_H) {
+Game::Game(int W_W, int W_H)
+{
     WINDOW_H = W_H;
     WINDOW_W = W_W;
 }
 
 Game::~Game() {}
 
-bool Game::init(const char *title) {
-    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+bool Game::init(const char *title)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) == 0)
+    {
         return false;
     }
 
@@ -29,16 +32,17 @@ bool Game::init(const char *title) {
 
     SDL_SetHint(SDL_HINT_VIDEO_WAYLAND_ALLOW_LIBDECOR, "0");
 
-
     window = SDL_CreateWindow(title, WINDOW_W, WINDOW_H, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-    if (!window) {
+    if (!window)
+    {
         return false;
     }
 
     context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, context);
 
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
+    {
         std::cerr << "Failed to initialize GLAD\n";
         return false;
     }
@@ -56,11 +60,11 @@ bool Game::init(const char *title) {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
     // glEnable(GL_BLEND);
     glEnable(GL_BLEND);
-glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     shader = LoadShader("shaders/vertex.glsl", "shaders/fragment.glsl");
 
@@ -68,36 +72,42 @@ glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     return true;
 }
 
-void Game::handleEvent() {
+void Game::handleEvent()
+{
     SDL_Event e;
-    while (SDL_PollEvent(&e)) {
-        if (e.type == SDL_EVENT_QUIT) {
+    while (SDL_PollEvent(&e))
+    {
+        if (e.type == SDL_EVENT_QUIT)
+        {
             isRunning = false;
         }
     }
 }
 
-void Game::update() {
-    
+void Game::update()
+{
 }
 
-void Game::render() {
+void Game::render()
+{
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-
     glUseProgram(shader);
     glBindVertexArray(vao);
-    
-    glUniform2f(glGetUniformLocation(shader,"WINDOW"),(float)WINDOW_W, (float)WINDOW_H);
-    glUniform2f(glGetUniformLocation(shader,"center"),360.0f, 360.0f);
-    glUniform1f(glGetUniformLocation(shader,"radius"),300.0f);
 
+    // glUniform2f(glGetUniformLocation(shader,"WINDOW"),(float)WINDOW_W, (float)WINDOW_H);
+    // glUniform2f(glGetUniformLocation(shader,"center"),360.0f, 360.0f);
+    // glUniform1f(glGetUniformLocation(shader,"radius"),300.0f);
 
-  
+    glUniform2f(glGetUniformLocation(shader, "WINDOW"), (float)WINDOW_W, (float)WINDOW_H);
+    glUniform3f(glGetUniformLocation(shader, "uCameraOrigin"), 0.0f, 0.0f, 0.0f);
+    glUniform1f(glGetUniformLocation(shader, "uViewportHeight"), 2.0f);
+    glUniform1f(glGetUniformLocation(shader, "uFocalLength"), 1.0f);
+
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-    SDL_GL_SwapWindow(window);  // Swap buffers
+    SDL_GL_SwapWindow(window); // Swap buffers
 
-    SDL_Delay(16);  
+    SDL_Delay(16);
 }
